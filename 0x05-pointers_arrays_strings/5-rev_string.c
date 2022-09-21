33f0ce3a01f45cca0e00399001310f9e159f613d
Vacus7 <syllaladjivacaba@gmail.com>
2022-09-20 18:16:02 -0700

    $message

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+{
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
+
+	while (s[fcounter] != '\0')
+		fcounter++;
+
+	for (i = 0; i < fcounter; i++)
+	{
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
+	}
+}
